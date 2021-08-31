779f68f20ad59c516496fd22be1b7b44e1a92636
DmanCoder <ktdenis.1@live.com>
2021-08-31 20:03:58 +1000

    created app styles

--- src/client/App.styles.ts
+++ src/client/App.styles.ts
@@ -1,10 +1,24 @@
 import styled from 'styled-components';
 import IconButton from '@material-ui/core/IconButton';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import { AppBar, Typography, withStyles } from '@material-ui/core';
 
 export const Wrapper = styled.div`
   margin: 40px;
 `;
+
+export const DialogImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const StyledDialogTitle = styled(DialogTitle)`
+  padding-left: 0;
+  padding-right: 0;
+`;
+
+export const DialogText = styled.p``;
 
 // export const StyledButton = styled(IconButton)`
 
@@ -12,20 +26,20 @@ export const Wrapper = styled.div`
 
 export const StyledButton = withStyles({
   label: {
-    flexDirection: "column"
-  }
+    flexDirection: 'column',
+  },
 })(IconButton);
 
 export const StyledAppBar = styled(AppBar)`
-  background : white;
+  background: white;
   margin-bottom: 15px;
   border-radius: 20px;
 `;
 
 export const HeaderTypography = withStyles({
   root: {
-    color: "black",
-    WebkitTextStroke: "0.5px darkgoldenrod",
-    fontStyle: "italic"
-  }
+    color: 'black',
+    WebkitTextStroke: '0.5px darkgoldenrod',
+    fontStyle: 'italic',
+  },
 })(Typography);
