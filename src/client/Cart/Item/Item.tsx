62bb86b15dc5c15e97deb4ef7c4cb2f05aad3e66
Abdul Ahmad <abdul.as.ahmad@gmail.com>
2021-10-29 18:08:40 +1000

    Add Dialog component for item cards

--- src/client/Cart/Item/Item.tsx
+++ src/client/Cart/Item/Item.tsx
@@ -1,25 +1,43 @@
-import Button from '@material-ui/core/Button';
+import { useState } from "react";
 // Types
-import { CartItemType } from '../../App';
+import { CartItemType } from "../../App";
 // Styles
-import { Wrapper } from './Item.styles';
+import { Wrapper } from "./Item.styles";
+// Components
+import { Button } from "@material-ui/core";
+import ItemDialog from "../ItemDialog/ItemDialog";
 
 type Props = {
   item: CartItemType;
   handleAddToCart: (clickedItem: CartItemType) => void;
 };
 
-const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
-  <Wrapper>
-    <img src={item.image} alt={item.title} />
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  return (
     <div>
-      <h3>{item.title}</h3>
-      <h3>${item.price}</h3>
+      <ItemDialog
+        item={item}
+        open={dialogOpen}
+        onClose={() => setDialogOpen(false)}
+        buttonOnClick={() => handleAddToCart(item)}
+      />
+      <Wrapper onClick={() => setDialogOpen(true)}>
+        <img src={item.image} alt={item.title} />
+        <div>
+          <h3>{item.title}</h3>
+          <h3>${item.price}</h3>
+        </div>
+        <Button
+          onClick={() => handleAddToCart(item)}
+          data-cy={`add-to-cart-${item.id}`}
+        >
+          Add to cart
+        </Button>
+      </Wrapper>
     </div>
-    <Button
-      onClick={() => handleAddToCart(item)}
-      data-cy={`add-to-cart-${item.id}`}>Add to cart</Button>
-  </Wrapper>
-);
+  );
+};
 
 export default Item;
